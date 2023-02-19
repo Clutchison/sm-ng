d62d1ab50828d42241c603d0a4f5d3af6639c651
Sean Hutchison <xskhutchx@gmail.com>
2023-02-18 19:05:20 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { test } from './tesseract/tess.service';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'sm-ng';
+  url = 'https://i.imgur.com/VaiOZ7G.png';
+  output = '';
+
+  @ViewChild('box') box: ElementRef<HTMLInputElement> | undefined = undefined;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.box?.nativeElement.setAttribute('value', this.url);
+  }
+
+  t() {
+    if (this.url) {
+      test(this.url).then(o => this.output = o);
+    }
+  }
+
+  onKey(v: string) {
+    this.url = v;
+  }
 }
---
+++ src/app/tesseract/tess.service.ts
@@ -0,0 +1,27 @@
+import { environment } from 'src/environments/environment';
+import * as Tesseract from 'tesseract.js';
+
+export const test = async (url: string): Promise<string> => {
+
+  console.log(environment.TESSDATA_PREFIX);
+  // console.log('Creating worker...')
+  const worker = await Tesseract.createWorker({
+  });
+  // console.log('Done!')
+  // console.log('Loading language...')
+  await worker.loadLanguage('eng');
+  // console.log('Done!')
+  // console.log('Initializing language...')
+  await worker.initialize('eng');
+  // console.log('Done!')
+  await worker.setParameters({
+    tessedit_pageseg_mode: Tesseract.PSM.SPARSE_TEXT,
+
+  })
+  // console.log('Recognizing text...');
+  const { data: { text } } = await worker.recognize(url);
+  // console.log('Done!')
+  console.log(text);
+  await worker.terminate();
+  return text;
+};
